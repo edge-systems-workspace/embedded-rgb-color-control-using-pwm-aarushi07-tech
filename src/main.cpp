3fcebb67aeb3d9dbf26bef19b42c88e0c056ab18
aarushi07-tech <aarushi.sharma_cs.aiml24@gla.ac.in>
2026-02-28 23:53:16 +0530

    final commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,46 @@
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author aarushi07-tech
+ * @date  2026-02-20
  *
  * @details
  * Controls RGB LED using digital ON/OFF
  * and analog PWM brightness control.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
-
- // TODO 2:
- // Define GREEN pin (Use 10)
-
- // TODO 3:
- // Define BLUE pin (Use 11)
-
+const int PIN_RED   = 5;
+const int PIN_GREEN = 6;
+const int PIN_BLUE  = 9;
 void setup() {
+    pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE,  OUTPUT);
 
-    // TODO 4:
-    // Initialize Serial communication (9600 baud)
 
-    // TODO 5:
-    // Configure RGB pins as OUTPUT
-
-    // TODO 6:
-    // Print initialization message
 }
 
 void loop() {
+    // color code #00C9CC (R = 0,   G = 201, B = 204)
+  analogWrite(PIN_RED,   0);
+  analogWrite(PIN_GREEN, 201);
+  analogWrite(PIN_BLUE,  204);
+
+  delay(1000); // keep the color 1 second
+
+  // color code #F7788A (R = 247, G = 120, B = 138)
+  analogWrite(PIN_RED,   247);
+  analogWrite(PIN_GREEN, 120);
+  analogWrite(PIN_BLUE,  138);
+
+  delay(1000); // keep the color 1 second
+
+  // color code #34A853 (R = 52,  G = 168, B = 83)
+  analogWrite(PIN_RED,   52);
+  analogWrite(PIN_GREEN, 168);
+  analogWrite(PIN_BLUE,  83);
+
+  delay(1000); // keep the color 1 second
 
     // -------- DIGITAL MODE --------
 
